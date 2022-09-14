427ae91ca3737556c5f7bbf9f7e6fe11fe7acb0e
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-14 20:49:08 +0200

    Update Ingredients table

--- models/Ingredient.py
+++ models/Ingredient.py
@@ -4,14 +4,13 @@ from sqlalchemy.sql import func
 from models.model_base import ModelBase
 
 
-class NutritionalAdditives(ModelBase):
-    __tablename__ = "nutritional_additives"
+class Ingredient(ModelBase):
+    __tablename__ = "Ingredients"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
 
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     name = Column(String(length=45), unique=True, nullable=False)
-    chemical_formula = Column(String(length=45), unique=True, nullable=False)
 
     def __repr__(self) -> str:
-        return f'<Nutritional additives: {self.name}>'
+        return f'<Ingredient: {self.name}>'
