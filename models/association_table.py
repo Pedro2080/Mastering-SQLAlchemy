f1a3106a4e4e04d88d81402738bd03a88e965e64
Joao Pedro da Silva <joaopedro2080@hotmail.com>
2022-09-16 12:02:43 +0200

    Update association table many to many relatioship tables

--- models/association_table.py
+++ models/association_table.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Table
 from models.model_base import ModelBase
+from models.ice_pop_type import IcePopType
+
 
 # invoices has many lotes
 
@@ -8,4 +10,28 @@ invoices_lotes = Table(
     ModelBase.metadata,
     Column("invoices_id", ForeignKey("invoices.id")),
     Column("lote_id", ForeignKey("lotes.id")),
+)
+
+# ice poo has many ingredients
+ice_pop_ingredients = Table(
+    "ice_pop_ingredients",
+    ModelBase.metadata,
+    Column("ice_pop_id", ForeignKey("ice_pops.id")),
+    Column("ingredient_id", ForeignKey("ingredients.id")),
+)
+
+# ice pop has many preservatives
+ice_pop_preservatives = Table(
+    "ice_pop_preservatives ",
+    ModelBase.metadata,
+    Column("ice_pop_id", ForeignKey("ice_pops.id")),
+    Column("preservative_id", ForeignKey("preservatives.id")),
+)
+
+# ice po has many nutritional additives
+ice_pop_nutritional_additives = Table(
+    "ice_pop_nutritional_additives ",
+    ModelBase.metadata,
+    Column("ice_pop_id", ForeignKey("ice_pops.id")),
+    Column("nutritional_additives_id", ForeignKey("nutritional_additives.id")),
 )
